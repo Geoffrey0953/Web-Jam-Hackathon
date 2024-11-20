7e32e94e6612e721b7452f2398be07224177f988
kellyserafico <seraficok@gmail.com>
2024-11-20 00:56:20 -0800

    add nav bar

--- dinesafe/src/components/SearchBar.jsx
+++ dinesafe/src/components/SearchBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Menu from "../assets/menu.svg";
 const SearchBar = () => {
   return (
-    <div className="w-96 absolute">
-      {/* <input type="text" placeholder="Search for a restaurant" /> */}
-      KSDLJFGLKSDJFKLSDFJKLSDF
+    <div className="w-96 bg-white z-40 rounded-full h-14 flex ml-4 shadow-lg">
+        <img src={Menu} alt="menu" className="w-6 h-6 m-4 float-left" />
+        <input className="" type="text" placeholder="Search..." />
     </div>
   );
 }
--- dinesafe/src/pages/Home.jsx
+++ dinesafe/src/pages/Home.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 import {APIProvider, Map} from '@vis.gl/react-google-maps';
+import SearchBar from '../components/SearchBar.jsx';
+import Logo from '../assets/logo.svg';
 
 const Home = () => (
   // console.log(import.meta.env.VITE_GOOGLE_MAPS_API_KEY),
-  <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-    <Map
-      style={{width: '100vw', height: '100vh'}}
-      defaultCenter={{lat: 22.54992, lng: 0}}
-      defaultZoom={3}
-      gestureHandling={'greedy'}
-      disableDefaultUI={true}
-    />
-  </APIProvider>
+  <div className="">
+    <div className="absolute top-0 left-0 w-full h-4 bg-gradient-to-b from-black/40 to-transparent pointer-events-none z-50"></div>
+    <div className="flex flex-row justify-between absolute w-full mt-4">
+      <SearchBar />
+      <img src={Logo} alt="logo" className="z-40 m-4" />
+    </div>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+      <Map
+        style={{width: '100vw', height: '100vh'}}
+        defaultCenter={{lat: 22.54992, lng: 0}}
+        defaultZoom={3}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+        options={{
+          draggableCursor: 'default', 
+          draggingCursor: 'grabbing', 
+        }}
+      />
+    </APIProvider>
+  </div>
 );
 
 
